bc5f43ade72751801c8baccfc4308edfd77b6ad9
Vishal Kuo <vishalkuo@hotmail.com>
2016-09-08 20:43:49 -0400

    Decoding

--- main.py
+++ main.py
@@ -21,18 +21,38 @@ def encode(s):
 			max_index += 1
 			current_run = c
 
-	# Check last string
+	# Check last run to see if it needs to be added to result
 	if len(current_run) > 0:
 		resultant_code.append(dictionary[current_run])
 
 	return resultant_code
 
+def decode(l):
+	# Reverse dictionary
+	dictionary = dict((c, chr(c)) for c in range(ASCII_SIZE))
+	max_index = ASCII_SIZE
+	# Add our first entry and set our run
+	s = dictionary[l[0]]
+	r = s
+	for n in l[1:]:
+		# Number doesn't exist, we add our current run to first elem
+		if n not in dictionary:
+			entry = s + s[0]
+		else:
+			entry = dictionary[n] 
+		r += entry
+		dictionary[max_index] = s + entry[0]
+		max_index += 1
+		s = entry
+
+	return r
+
 
 def main():
-	# if len(sys.argv) < 2:
-	# 	raise ValueError("No string provided provided!")
-	# s_to_encode = sys.argv[1]
-	print(encode("thisisthe"))
+	if len(sys.argv) < 2:
+		raise ValueError("No string provided provided!")
+	s_to_encode = sys.argv[1]
+	print(encode(s_to_encode))
 
 
 if __name__ == '__main__':
