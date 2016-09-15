45bd7853d848187ca2d91105490d3e1f565dff74
Vishal Kuo <vishalkuo@hotmail.com>
2016-09-14 20:54:38 -0400

    Serialize/deserialize using pickle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import argparse
+import pickle
+import math
 
 ASCII_SIZE = 256
 
@@ -9,57 +11,61 @@ parser.add_argument('-c', '--compress', help='Compress a string or a file', acti
 parser.add_argument('source', metavar='S', type=str, nargs='?', help='input to compress or decompress')
 
 def encode(s):
-	# Produce dict keyed by ascii characters with appropriate index value
-	dictionary = dict((chr(c), c) for c in range(ASCII_SIZE))
-	max_index = ASCII_SIZE
-	resultant_code = []
-	current_run = ""
-	for c in s:
-		# Set our run to the letter in the string + our current run
-		run = c + current_run
-		# Case when we already have this in our dict (can be compressed further)
-		if run in dictionary:
-			current_run = run
-		else:
-			# Update dict and result, reset current run to our current character
-			dictionary[run] = max_index
-			resultant_code.append(dictionary[current_run])
-			max_index += 1
-			current_run = c
+  # Produce dict keyed by ascii characters with appropriate index value
+  dictionary = dict((chr(c), c) for c in range(ASCII_SIZE))
+  max_index = ASCII_SIZE
+  resultant_code = []
+  current_run = ""
+  for c in s:
+    # Set our run to the letter in the string + our current run
+    run = c + current_run
+    # Case when we already have this in our dict (can be compressed further)
+    if run in dictionary:
+      current_run = run
+    else:
+      # Update dict and result, reset current run to our current character
+      dictionary[run] = max_index
+      resultant_code.append(dictionary[current_run])
+      max_index += 1
+      current_run = c
 
-	# Check last run to see if it needs to be added to result
-	if len(current_run) > 0:
-		resultant_code.append(dictionary[current_run])
-
-	return resultant_code
+  # Check last run to see if it needs to be added to result
+  if len(current_run) > 0:
+    resultant_code.append(dictionary[current_run])
+  
+  with open('output.dat', 'wb') as f:
+    pickle.dump(resultant_code, f, pickle.HIGHEST_PROTOCOL)
 
 def decode(l):
-	# Reverse dictionary
-	dictionary = dict((c, chr(c)) for c in range(ASCII_SIZE))
-	max_index = ASCII_SIZE
-	# Add our first entry and set our run
-	s = dictionary[l[0]]
-	r = s
-	for n in l[1:]:
-		# Number doesn't exist, we add our current run to first elem
-		if n not in dictionary:
-			entry = s + s[0]
-		else:
-			entry = dictionary[n] 
-		r += entry
-		dictionary[max_index] = s + entry[0]
-		max_index += 1
-		s = entry
+  # Reverse dictionary
+  dictionary = dict((c, chr(c)) for c in range(ASCII_SIZE))
+  max_index = ASCII_SIZE
+  # Add our first entry and set our run
+  s = dictionary[l[0]]
+  r = s
+  for n in l[1:]:
+    # Number doesn't exist, we add our current run to first elem
+    if n not in dictionary:
+      entry = s + s[0]
+    else:
+      entry = dictionary[n] 
+    r += entry
+    dictionary[max_index] = s + entry[0]
+    max_index += 1
+    s = entry
 
-	return r
+  return r
 
 
 def main():
     args = vars(parser.parse_args())
     if args['decompress']:
-        print(decode(args['source']))
+        with open(args['source'], 'rb') as f:
+          result = pickle.load(f)
+        print(decode(result))
     else:
-        print(encode(args['source']))
+        res = encode(args['source'])
+
 if __name__ == '__main__':
-	main()
-	
+  main()
+  
